c06894a3fc809a32b952b1cf310d7d22b8242ff7
Caleb <56181475+7248510@users.noreply.github.com>
2021-11-20 00:45:05 -0500

    Update test.java

--- classes/test.java
+++ classes/test.java
@@ -1,13 +1,25 @@
 package com.company;
 
 public class test {
-    public static void test()
+    public static void testCall()
     {
-        System.out.println("Calling test");
+        System.out.println("Calling test from the class test");
     }
     public void test2()
     {
         System.out.println("Calling test2 without the static keyword");
     }
 
+    private int add()
+    {
+        return 1 + 3;
+    }
+    public void access()
+    {
+        System.out.print("\nCalling the public method access:");
+        testCall();
+        test2();
+        int i = add();
+        System.out.println("Calling add function: " + i );
+    }
 }
