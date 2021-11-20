0fef9cbf08f9b959a8337ba9a3eabfc85884d6ab
Caleb <56181475+7248510@users.noreply.github.com>
2021-11-20 00:44:42 -0500

    Update Main.java

--- classes/Main.java
+++ classes/Main.java
@@ -1,12 +1,29 @@
 package com.company;
 
 
-public class Main {
 
+public class Main {
+    public static void callMainS()
+    {
+        System.out.println("This is static test from main");
+    }
+    public void callMainNS()
+    {
+        System.out.println("This is non static test from main");
+    }
     public static void main(String[] args) {
-	System.out.println("Hello World!\nWe are now learning Java using OPENJDK 16");
+	    System.out.println("Hello World!\nWe are now learning Java using OPENJDK 16");
+        test.testCall();
         test hello = new test(); //Really similar to C++ minus the new keyword
-        hello.test();
-        hello.test2();
+        //W3 schools states(paraphrasing) that the static keyword allows functions to be called without creating objects or ^
+
+        hello.access();
+        callMainS();
+        //callMainNS(); //This function cannot be called due to it not being static. It needs an object
+        Main getNS = new Main();
+        getNS.callMainNS();
+
     }
 }
+
+
