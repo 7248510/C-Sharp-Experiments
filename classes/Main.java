092e9fb0df9ab4cb531faabf57f02af080c48ed0
Caleb <56181475+7248510@users.noreply.github.com>
2021-11-20 00:13:43 -0500

    Add files via upload

---
+++ classes/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World!\nWe are now learning Java using OPENJDK 16");
+        test hello = new test(); //Really similar to C++ minus the new keyword
+        hello.test();
+        hello.test2();
+    }
+}
---
+++ classes/test.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class test {
+    public static void test()
+    {
+        System.out.println("Calling test");
+    }
+    public static void test2()
+    {
+        System.out.println("Calling test2");
+    }
+
+}
